2c9a6459b15ad5c4e9a320f80930a77e81cfe6e9
Alex <alexander.kono.oliveira@gmail.com>
2025-04-16 16:53:25 -0300

    modified:   7_1.c

--- 7_1.c
+++ 7_1.c
@@ -7,7 +7,7 @@ void main(){
     scanf("%d", &n);
     scanf("%lf", &x);
 
-    for (int i = 1; i <= n; i++){
+    for (int i = 1; i < n; i++){
         parcela = -parcela*x*x/((2*i)*((2*i)-1));
         cosseno += parcela;
     }

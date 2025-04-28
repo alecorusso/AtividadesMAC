1d3fb44ca38f4b99f15dea7cad18d9cf9948f4f0
Alex <alexander.kono.oliveira@gmail.com>
2025-04-27 22:42:32 -0300

    falta o 7.2

--- 7_2_VERIFICAR.c
+++ 7_2_VERIFICAR.c
@@ -12,12 +12,15 @@ void main(){
 
     seno = x;
     parcela = x;
+
+    printf("Parcela 1: %6.3f\n", parcela);
+    printf("Seno 1: %6.3f\n", seno);
     
     for (int i = 1; i < n; i++){
-        //printf("%f\n", x*x);
-        //printf("%f\n", x*x/((2*i)*((2*i)+1)));
         parcela = -parcela*x*x/((2*i)*((2*i)+1));
         seno += parcela;
+        printf("Parcela %d: %6.3f\n", i+1, parcela);
+        printf("Seno %d: %6.3f\n", i+1, seno);
     }
     
     printf("%1.1f :  %6.3f =   %6.3f\n", x, seno, sin(x));

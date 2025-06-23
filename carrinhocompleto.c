1423ef55cf225af39eeb4ce5bac6d5b3703bc8ef
alecorusso <alexander.kono.oliveira@gmail.com>
2025-06-23 05:03:42 -0300

    Update carrinhocompleto.c

--- carrinhocompleto.c
+++ carrinhocompleto.c
@@ -11,12 +11,11 @@
 #define TPM2_IRQ_LINE TPM2_IRQn  // relaciona a interrupção ao timer TPM2
 #define TPM_IRQ_PRIORITY 1      // define a prioridade da interrupção
 #define pinoTrigger 1 //PTB1
-#define pinoEchoUP 20 //PTE20
-#define pinoEchoDOWN 2 //PTB2
+#define pinoEchoDOWN 20 //PTE20
 #define TPM_MOD_ULTRASSOM 37500
 #define frequencia_PWM 0.01 // ciclos/ms
 #define velocidade_do_som 34 //cm/ms
-volatile uint16_t instante_up = 0; // Armazena instante (em ticks) da borda de subida do sinal de Echo
+volatile uint16_t instante_up = 846; // 846 << tick alto << 847 (aproximadamente constante)
 volatile uint16_t instante_down = 0; // Armazena instante (em ticks) da borda de descida do sinal de Echo
 
 // Sensores Infravermelho e motores
@@ -30,13 +29,7 @@ uint16_t duty_motor = 3500;
 void tpm1_isr(void *arg)
 {
        TPM1->STATUS |= TPM_STATUS_CH0F_MASK; // zera a flag que gerou a interrupção
-       instante_up = TPM1->CONTROLS[0].CnV; // coloca o valor atual do timer na variável "captured"l
-}
-
-void tpm2_isr(void *arg)
-{
-       TPM2->STATUS |= TPM_STATUS_CH0F_MASK; // zera a flag que gerou a interrupção
-       instante_down = TPM2->CONTROLS[0].CnV; // coloca o valor atual do timer na variável "captured"l
+       instante_down = TPM1->CONTROLS[0].CnV; // coloca o valor atual do timer na variável "captured"l
 }
 
 void main(void)
@@ -55,22 +48,12 @@ void main(void)
     // Conecta a interrupção via Zephyr para o TPM 1
     IRQ_CONNECT(TPM1_IRQ_LINE, TPM_IRQ_PRIORITY, tpm1_isr, NULL, 0);
     irq_enable(TPM1_IRQ_LINE);
- 
-    // Conecta a interrupção via Zephyr para o TPM 2 
-    IRQ_CONNECT(TPM2_IRQ_LINE, TPM_IRQ_PRIORITY, tpm2_isr, NULL, 0);
-    irq_enable(TPM2_IRQ_LINE);
 
     // Inicializa TPM1 para ultrassom com módulo e prescaler desejado
     pwm_tpm_Init(TPM1, TPM_PLLFLL, TPM_MOD_ULTRASSOM, TPM_CLK, PS_128, EDGE_PWM);
 
-    // Inicializa TPM2 para ultrassom com módulo e prescaler desejado
-    pwm_tpm_Init(TPM2, TPM_PLLFLL, TPM_MOD_ULTRASSOM, TPM_CLK, PS_128, EDGE_PWM);
-
-    // Configura canal 0 do TPM1 como Output Echo (input capture na borda de subida)
-    pwm_tpm_Ch_Init(TPM1, 0, TPM_INPUT_CAPTURE_RISING| TPM_CHANNEL_INTERRUPT, GPIOE, pinoEchoUP);
-
-    // Configura canal 0 do TPM2 como Output Echo (input capture na borda de descida)
-    pwm_tpm_Ch_Init(TPM2, 0, TPM_INPUT_CAPTURE_FALLING| TPM_CHANNEL_INTERRUPT, GPIOB, pinoEchoDOWN);
+    // Configura canal 0 do TPM1 como Output Echo (input capture na borda de descida)
+    pwm_tpm_Ch_Init(TPM1, 0, TPM_INPUT_CAPTURE_FALLING| TPM_CHANNEL_INTERRUPT, GPIOE, pinoEchoUP);
 
     // Configura canal 1 do TPM1 para trigger do sinal de ultrassom
     pwm_tpm_Ch_Init(TPM1, 1, TPM_PWM_H, GPIOB, pinoTrigger);
@@ -97,7 +80,9 @@ void main(void)
     gpio_pin_configure(portaE, INPUT_PIN_ESQUERDO, GPIO_INPUT); // Define sensor esquedo como input
 
     while (1){
-        while(distancia > 20){
+           //TICK DE 20 CM ~ 1294
+           //DURAÇÃO DE 20 CM - 448
+        while(duracao_sinal > 448){
             duracao_sinal = instante_down - instante_up;
             duracao = (duracao_sinal/37500.0)/frequencia_PWM;
             distancia = duracao*velocidade_do_som/2;
@@ -107,36 +92,72 @@ void main(void)
             printf("Duração (milisegundos): %f\n", duracao);
             printf("Distancia (centimetros): %f\n", distancia);
             printk("\n");*/
-            valDireito = !gpio_pin_get(portaE, INPUT_PIN_DIREITO);
-            valEsquerdo = !gpio_pin_get(portaC, INPUT_PIN_ESQUERDO);
-
-            pwm_tpm_CnV(TPM0, 1, 0);          // Motor direito pra frente
-            pwm_tpm_CnV(TPM0, 2, duty_motor);
-
-            pwm_tpm_CnV(TPM0, 3, 0);          // Motor esquerdo para frente
-            pwm_tpm_CnV(TPM0, 4, duty_motor);
-
-            while(valDireito == 0){
-                //printk("entrou regresso direito\n");
-                pwm_tpm_CnV(TPM0, 1, duty_motor);
-                pwm_tpm_CnV(TPM0, 2, 0);
-                valDireito = !gpio_pin_get(portaE, INPUT_PIN_DIREITO);
-                valEsquerdo = !gpio_pin_get(portaC, INPUT_PIN_ESQUERDO);
-            }
-        
-            while(valEsquerdo == 0){
-                //printk("entrou regresso esquerdo\n");
-                pwm_tpm_CnV(TPM0, 3, duty_motor);
-                pwm_tpm_CnV(TPM0, 4, 0);
-                valDireito = !gpio_pin_get(portaE, INPUT_PIN_DIREITO);
-                valEsquerdo = !gpio_pin_get(portaC, INPUT_PIN_ESQUERDO);
-            }
-            //printk("Valor do PTE20: %d\n", valDireito);
-            //printk("Valor do PTC6: %d\n", valEsquerdo);
-            //k_msleep(500);
-                k_msleep(500);
-        }
-        while(distancia <= 20){
+            while (duracao_sinal > 448) {
+                   valDireito = gpio_pin_get(portaE, INPUT_PIN_DIREITO);
+                   valEsquerdo = gpio_pin_get(portaC, INPUT_PIN_ESQUERDO);
+                   if(valEsquerdo == 0 && valDireito == 0){
+                          pwm_tpm_CnV(TPM0, 1, 0);          // Motor direito pra frente
+                          pwm_tpm_CnV(TPM0, 2, duty_motor);
+                          pwm_tpm_CnV(TPM0, 3, 0);          // Motor esquerdo para frente
+                          pwm_tpm_CnV(TPM0, 4, duty_motor);
+                          //printk("Valor do direito: %d\n", valDireito);
+                          //printk("Valor do esquerdo: %d\n", valEsquerdo);
+                          //printk("---------------------------\n");
+                          //k_msleep(200);
+                   }
+       
+                   if(valDireito == 1 && valEsquerdo == 0){
+                       //printk("entrou regresso direito\n");
+                       pwm_tpm_CnV(TPM0, 1, duty_motor);
+                       pwm_tpm_CnV(TPM0, 2, 0);
+                       pwm_tpm_CnV(TPM0, 3, 0);          
+                       pwm_tpm_CnV(TPM0, 4, duty_motor);
+                       valDireito = gpio_pin_get(portaE, INPUT_PIN_DIREITO);
+                       valEsquerdo = gpio_pin_get(portaC, INPUT_PIN_ESQUERDO);
+                       printk("Valor do direito: %d\n", valDireito);
+                       //printk("Valor do direito: %d\n", valDireito);
+                       //printk("Valor do esquerdo: %d\n", valEsquerdo);
+                       //printk("---------------------------\n");
+                       //k_msleep(200);
+                   }
+               
+                   else if(valEsquerdo == 1 && valDireito == 0){
+                       //printk("entrou regresso esquerdo\n");
+                       pwm_tpm_CnV(TPM0, 1, 0);         
+                       pwm_tpm_CnV(TPM0, 2, duty_motor);
+                       pwm_tpm_CnV(TPM0, 3, duty_motor);
+                       pwm_tpm_CnV(TPM0, 4, 0);
+                       valDireito = gpio_pin_get(portaE, INPUT_PIN_DIREITO);
+                       valEsquerdo = gpio_pin_get(portaC, INPUT_PIN_ESQUERDO);
+                       printk("Valor do direito: %d\n", valDireito);
+                       //printk("Valor do direito: %d\n", valDireito);
+                       //printk("Valor do esquerdo: %d\n", valEsquerdo);
+                       //printk("---------------------------\n");
+                       //k_msleep(200);
+                       
+                   }
+       
+                   else if(valEsquerdo == 0 && valDireito == 0){
+                       //printk("entrou regresso ambos\n");
+                       pwm_tpm_CnV(TPM0, 1, duty_motor);
+                       pwm_tpm_CnV(TPM0, 2, 0);
+                       pwm_tpm_CnV(TPM0, 3, duty_motor);
+                       pwm_tpm_CnV(TPM0, 4, 0);
+                       valDireito = gpio_pin_get(portaE, INPUT_PIN_DIREITO);
+                       valEsquerdo = gpio_pin_get(portaC, INPUT_PIN_ESQUERDO);
+                       //printk("Valor do direito: %d\n", valDireito);
+                       //printk("Valor do esquerdo: %d\n", valEsquerdo);
+                       //printk("---------------------------\n");
+                       //k_msleep(200);
+                       
+                   }
+       
+                   //printk("Valor do direito: %d\n", valDireito);
+                   //printk("Valor do esquerdo: %d\n", valEsquerdo);
+                   //printk("---------------------------\n");
+                   //k_msleep(200);
+               }
+        while(duracao_sinal <= 448){
             pwm_tpm_CnV(TPM0, 1, 0);          
             pwm_tpm_CnV(TPM0, 2, 0);
             pwm_tpm_CnV(TPM0, 3, 0);          
@@ -145,7 +166,7 @@ void main(void)
             duracao_sinal = instante_down - instante_up;
             duracao = (duracao_sinal/37500.0)/frequencia_PWM;
             distancia = duracao*velocidade_do_som/2;
-            k_msleep(500);
+            //k_msleep(500);
         }
     }
 }

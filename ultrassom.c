48e378d5a927af86e52287fe5761d865bb6df00a
alecorusso <alexander.kono.oliveira@gmail.com>
2025-06-23 03:54:14 -0300

    Update ultrassom.c

--- ultrassom.c
+++ ultrassom.c
@@ -6,25 +6,18 @@
 #define TPM2_IRQ_LINE TPM2_IRQn  // relaciona a interrupção ao timer TPM2
 #define TPM_IRQ_PRIORITY 1      // define a prioridade da interrupção
 #define pinoTrigger 1 //PTB1
-#define pinoEchoUP 20 //PTE20
-#define pinoEchoDOWN 2 //PTB2
+#define pinoEchoDOWN 20 //PTE20
 #define TPM_MOD 37500
-#define frequencia_PWM 10 //Hz
+#define frequencia_PWM 10 // 
 #define velocidade_do_som 34000 //cm/s
 
-volatile uint16_t instante_up = 0;
+volatile uint16_t instante_up = 846; // 846 << tick alto << 847
 volatile uint16_t instante_down = 0;
 
 void tpm1_isr(void *arg)
 {
        TPM1->STATUS |= TPM_STATUS_CH0F_MASK; // zera a flag que gerou a interrupção
-       instante_up = TPM1->CONTROLS[0].CnV; // coloca o valor atual do timer na variável "captured"l
-}
-
-void tpm2_isr(void *arg)
-{
-       TPM2->STATUS |= TPM_STATUS_CH0F_MASK; // zera a flag que gerou a interrupção
-       instante_down = TPM2->CONTROLS[0].CnV; // coloca o valor atual do timer na variável "captured"l
+       instante_down = TPM1->CONTROLS[0].CnV; // coloca o valor atual do timer na variável "instante_down"
 }
 
 void main(void)
@@ -32,21 +25,12 @@ void main(void)
     // Conecta a interrupção via Zephyr para o TPM 1
     IRQ_CONNECT(TPM1_IRQ_LINE, TPM_IRQ_PRIORITY, tpm1_isr, NULL, 0);
     irq_enable(TPM1_IRQ_LINE);
- 
-    // Conecta a interrupção via Zephyr para o TPM 2 
-    IRQ_CONNECT(TPM2_IRQ_LINE, TPM_IRQ_PRIORITY, tpm2_isr, NULL, 0);
-    irq_enable(TPM2_IRQ_LINE);
 
     // Inicializa TPM1 com módulo e prescaler desejado
     pwm_tpm_Init(TPM1, TPM_PLLFLL, TPM_MOD, TPM_CLK, PS_128, EDGE_PWM);
 
-    // Inicializa TPM2 com módulo e prescaler desejado
-    pwm_tpm_Init(TPM2, TPM_PLLFLL, TPM_MOD, TPM_CLK, PS_128, EDGE_PWM);
-
     // Configura canal 0 do TPM1 como Output Echo (input capture na borda de subida)
-    pwm_tpm_Ch_Init(TPM1, 0, TPM_INPUT_CAPTURE_RISING| TPM_CHANNEL_INTERRUPT, GPIOE, pinoEchoUP);
-
-    pwm_tpm_Ch_Init(TPM2, 0, TPM_INPUT_CAPTURE_FALLING| TPM_CHANNEL_INTERRUPT, GPIOB, pinoEchoDOWN);
+    pwm_tpm_Ch_Init(TPM1, 0, TPM_INPUT_CAPTURE_FALLING| TPM_CHANNEL_INTERRUPT, GPIOE, pinoEchoDOWN);
 
     // Configura canal 1 do TPM1 para trigger do sinal
     pwm_tpm_Ch_Init(TPM1, 1, TPM_PWM_H, GPIOB, pinoTrigger);
@@ -59,8 +43,8 @@ void main(void)
     float distancia = 0;
     while (1)
     {
-        duracao_sinal = instante_up - instante_down;
-        duracao = (duracao_sinal/TPM_MOD)/frequencia_PWM;
+        duracao_sinal = instante_down - instante_up;
+        duracao = (duracao_sinal/37500.0)/frequencia_PWM;
         distancia = duracao*velocidade_do_som/2;
         printk("Instante ALTO (tick): %d\n", instante_up);
         printk("Instante BAIXO (tick): %d\n", instante_down);
@@ -71,3 +55,5 @@ void main(void)
         k_msleep(500);
     }
 }
+
+//TICK DE 20 CM ~ 1294
